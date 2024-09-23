3c1a4fd89455a714351e5d5ba253ce77b7bc2877
GeneralRakokot <55301517+GeneralRakokot@users.noreply.github.com>
2024-09-23 16:38:28 +0300

    Add files via upload

--- ClipControl.cs
+++ ClipControl.cs
@@ -1,23 +1,60 @@
 using UnityEngine;
+using System.Collections;
 
 public class ClipControl : MonoBehaviour
 {
-    AudioSource audioSource;
+    private AudioSource audioSource;
+    private bool idle;
+    private IEnumerator coroutine;
 
     public void Unfade(float time)
     {
-        audioSource.Play();
+        if (coroutine != null) StopCoroutine(coroutine);
+        coroutine = VolumeCorutine(1, time);
+        StartCoroutine(coroutine);
     }
 
     public void Fade(float time)
     {
-        audioSource.Stop();
+        if (coroutine != null) StopCoroutine(coroutine);
+        coroutine = VolumeCorutine(0, time);
+        StartCoroutine(coroutine);
     }
 
-   public void SetMusic(AudioClip clip)
+
+    private IEnumerator VolumeCorutine(float endVal, float duration)
+    {
+        float percent = 0f;
+
+        if (endVal != 0f)
+        {
+            audioSource.Play();
+            idle = false;
+        }
+
+        while (percent < 1)
+        {
+            percent += Time.deltaTime * 1 / duration;
+            audioSource.volume = Mathf.Lerp(audioSource.volume, endVal, percent * percent);
+            yield return null;
+        }
+
+        if (endVal == 0f)
+        {
+            audioSource.Stop();
+            idle = true;
+        }
+    }
+
+    public void SetMusic(AudioClip clip)
     {
         audioSource = gameObject.AddComponent<AudioSource>();
         audioSource.loop = true;
         audioSource.clip = clip;
+    }
+
+    public bool IsIdle()
+    {
+        return idle;
     }
 }
--- MusicCore.cs
+++ MusicCore.cs
@@ -1,4 +1,6 @@
+//using System;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 using static MusicLibrary;
@@ -9,9 +11,11 @@ public static class MusicCore
     private static Dictionary<string, AudioClip> tracks = new Dictionary<string, AudioClip>();
     private static Dictionary<string, ClipControl> clipControls = new Dictionary<string, ClipControl>();
     private static string lastChangedName;
+    private static float lastChangedFadeTime;
 
-    public static void Play(string name, float fade = 0f)
+    public static void Play(string name, float fadeTime = 1f)
     {
+        Debug.Log("Play");
         if (!IsMusicChanged(name))
         {
             //Debug.Log("No music changes");
@@ -19,37 +23,24 @@ public static class MusicCore
         }
         if (!tracks.ContainsKey(name))
         {
+            lastChangedName = name;
+            lastChangedFadeTime = fadeTime;
             //Debug.Log("Music not found");
             return;
         }
 
+        FadeAll(fadeTime);
+
         AudioClip clip = tracks[name];
         ClipControl clipControl = GetClipControl(name, clip);
 
-        foreach (ClipControl _clipControl in  clipControls.Values )
-        {
-            _clipControl.Fade(fade);
-        }
+        clipControl.Unfade(fadeTime);
+    }
 
-        clipControl.Unfade(fade);
-
-        //if (isFating)
-        //{
-        //    EndFating();
-        //    main_MusicSource.clip = Resources.Load<AudioClip>(MusicName);
-        //    main_MusicSource.Play();
-        //}
-        //else if (fateTime == 0)
-        //{
-        //    main_MusicSource.clip = Resources.Load<AudioClip>(MusicName);
-        //    main_MusicSource.Play();
-        //}
-        //else
-        //{
-        //    auxiliary_MusicSource.clip = Resources.Load<AudioClip>(MusicName);
-        //    auxiliary_MusicSource.Play();
-        //    StartFating(fateTime);
-        //}
+    public static void Stop(float fadeTime = 1f)
+    {
+        FadeAll(fadeTime);
+        lastChangedName = null;
     }
 
     private static bool IsMusicChanged(string name)
@@ -64,10 +55,17 @@ public static class MusicCore
 
     public static void SetTracksLibrary(Track[] _tracks)
     {
+        ClearIdle();
         foreach (Track track in _tracks)
         {
             tracks[track.MusicName] = track.clip;
+            if (lastChangedName == track.MusicName)
+            {
+                lastChangedName = null;
+                Play(track.MusicName, lastChangedFadeTime);
+            }
         }
+
     }
 
     private static ClipControl GetClipControl(string name, AudioClip clip)
@@ -92,89 +90,25 @@ public static class MusicCore
         return clipControl;
     }
 
+    private static void ClearIdle()
+    {
+        foreach (ClipControl clipControl in clipControls.Values)
+        {
+            if (clipControl.IsIdle()) GameObject.Destroy(clipControl.gameObject);
+        }
+    }
 
-
-
-    //private static AudioSource main_MusicSource;
-    //private static AudioSource auxiliary_MusicSource;
-
-    //private static float fateTimeLeft = 0f;
-    //private static bool isFating = false;
-
-    //public static void Enter()
-    //{
-    //    {
-    //        main_MusicSource = CreateAudioSource("MainMusic");
-    //        auxiliary_MusicSource = CreateAudioSource("AuxMusic");
-    //    }
-    //}
-
-    //public static void Play(string MusicName, float fateTime = 2f)
-    //{
-    //    if (isFating) 
-    //    {
-    //        EndFating();
-    //        main_MusicSource.clip = Resources.Load<AudioClip>(MusicName);
-    //        main_MusicSource.Play();
-    //    }
-    //    else if(fateTime == 0)
-    //    {
-    //        main_MusicSource.clip = Resources.Load<AudioClip>(MusicName);
-    //        main_MusicSource.Play();
-    //    }
-    //    else
-    //    {
-    //        auxiliary_MusicSource.clip = Resources.Load<AudioClip>(MusicName);
-    //        auxiliary_MusicSource.Play();
-    //        StartFating(fateTime);
-    //    }
-    //}
-
-    //private static void StartFating(float time)
-    //{
-    //    auxiliary_MusicSource.volume = 0f;
-    //    fateTimeLeft = time;
-    //    isFating = true;
-    //    //MonoBehAnchor.OnTimeUpdated += Update;
-    //}
-
-    //private static void EndFating()
-    //{
-    //    main_MusicSource.Stop();
-    //    main_MusicSource.volume = 1f;
-
-    //    auxiliary_MusicSource.volume = 1f;
-
-    //    AudioSource buffer = main_MusicSource;
-    //    main_MusicSource = auxiliary_MusicSource;
-    //    auxiliary_MusicSource = buffer;
-    //    isFating = false;
-    //    //MonoBehAnchor.OnTimeUpdated -= Update;
-    //}
-
-    //private static void Update()
-    //{
-    //    fateTimeLeft -= Time.deltaTime;
-
-    //    main_MusicSource.volume = Mathf.Lerp(main_MusicSource.volume, 0f, Time.deltaTime / fateTimeLeft);
-    //    auxiliary_MusicSource.volume = Mathf.Lerp(auxiliary_MusicSource.volume, 1f, Time.deltaTime / fateTimeLeft);
-
-    //    if (fateTimeLeft < 0)
-    //    {
-    //        EndFating();
-    //    }
-    //}
-
-    //private static AudioSource CreateAudioSource(string name)
-    //{
-    //    GameObject EmptyObject = new GameObject(name);
-    //    //EmptyObject.transform.parent = Global.Anchor.transform;
-    //    return EmptyObject.AddComponent<AudioSource>();
-    //}
-
-    //private static bool isPlaying()
-    //{
-    //    if (main_MusicSource.isPlaying || auxiliary_MusicSource.isPlaying) { return true; }
-    //    return false;
-    //}
+    private static void FadeAll(float time)
+    {
+        if (MusicRoot == null || MusicRoot.transform.childCount == 0) return;
+        foreach (int i in Enumerable.Range(0, MusicRoot.transform.childCount))
+        {
+            Transform obj = MusicRoot.transform.GetChild(i);
+            ClipControl clipControl = obj.gameObject.GetComponent<ClipControl>();
+            if (clipControl != null)
+            {
+                clipControl.Fade(time);
+            }
+        }
+    }
 }
